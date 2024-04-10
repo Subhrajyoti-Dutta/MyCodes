14ec1e814b20d8512dbf3a9a50142eb8ae48da59
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2024-04-10 23:42:52 +0530

    (GET-DATE)

--- python/rough1.py
+++ python/rough1.py
@@ -1,33 +1,24 @@
-<<<<<<< HEAD
-a="""def KNN(class1,class2,x_new):\n
-  dist1 = np.sum((np.transpose(x_new)-class1)**2,axis=1)  \n
-  dist2 = np.sum((np.transpose(x_new)-class2)**2,axis=1)\n
-  dist1 = np.append([dist1],np.ones((1,5)),axis=0)\n
-  dist2 = np.append([dist2],2*np.ones((1,5)),axis=0)\n
-  dist  = np.concatenate((dist1, dist2), axis=1)\n
-  ii = np.argsort(dist[0])\n
-  dist = dist[:,ii]\n
-  if np.sum(dist[1][0:3])<=4:\n
-    return 1\n
-  else:\n
-    return 2\n
-\n
-# validate space\n
-#ensure the student code runs without an error\n
-try:\n
-  knn(class1,class2,x_new)\n
-except Exception as e:\n
-  print(e)\n
-  exit()\n
-\n"""
-a.split("\n\n")
-print("\n".join(a.split("\n\n")))
-=======
-import numpy as np
-
-print(np.add(np.array([1]), np.array([])))  # outputs empty array
-
-# print(np.add(np.array([1, 2]), np.array([])))  # raises ValueError
-
-print(np.source(np.add))
->>>>>>> dd03edc64e89a4288145cb993c5c8243bfcab11c
+a = """Displacement ( cc )
+Max Power
+Max Torque
+Mileage ( kmpl )
+Top Speed
+Riding Modes
+Transmission
+Transmission Type
+Gear Shifting Pattern
+Cylinders
+Bore
+Stroke
+Valves Per Cylinder
+Compression Ratio
+Ignintion
+Spark Plugs
+Colling System
+Clutch
+Fuel Delivery System
+Fuel Capacity ( Litre )
+Reserve Fule Capacity (litre)
+Emission Standard
+Fuel Type"""
+print(*a.split("\n"),sep="\",\"")
--- python/rough2.py
+++ python/rough2.py
@@ -1,11 +1,59 @@
-import numpy as np
+a = """    def prims(a_list):
+        inf = 1 + max([d for u in a_list.keys()
+                for (v, d) in a_list[u]])
 
-arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [8, 9, 10]
-]
+    visited = {}; distance = {}; nbr = {}
 
-a = np.array(arr)
+    for v in a_list.keys():
+        visited[v] = False
+        distance[v] = inf
+        nbr[v] = -1
 
-print(a)
+    visited[1] = True
+
+    for (v,d) in a_list[1]:
+        distance[v] = d
+        nbr[v] = 1
+
+    for i in range(1, len(a_list.keys())):
+        next_d = min([distance[v] for v in a_list.keys()
+                      if not visited[v]])
+        next_vlist = [v for v in a_list.keys()
+                      if (not visited[v] and distance[v] == next_d)]
+
+        next_v = min(next_vlist)
+
+        visited[next_v] = True
+
+        for (v,d) in a_list[next_v]:
+            if not visited[v]:
+                if d <= distance[v]:
+                    distance[v] = d
+                    nbr[v] = next_v
+
+    connected = True
+    TE = []
+
+    for u in nbr.keys():
+        if u != 1:
+            if nbr[u] == -1:
+                connected = False
+                break
+            else:
+                TE.append((nbr[u], u))
+
+    if not connected:
+        print("The given graph is not connected.")
+    else:
+        print("The edges of the MST are:")
+
+        for e in TE:
+            print(e)
+
+        cost = sum([d for u in a_list.keys()
+                    for (v,d) in a_list[u]
+                    if v == nbr[u]])
+
+        print("Minimum Cost is ", cost)"""
+
+print("\n".join(map(lambda x : x[4:],a.split("\n"))))
