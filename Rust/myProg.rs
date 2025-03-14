8942bfa1d2c1b794d012e4e5ce80f6696627450f
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2025-03-14 12:23:34 +0530

    Subhrajyoti: 2025-03-14 12:23:34

--- Rust/myProg.rs
+++ Rust/myProg.rs
@@ -9,7 +9,6 @@
 fn main() {
     let n = 9;  // No need for `mut`
     let t = true;
-    let y = !t;
-    let _u = Vec::<i32>::new();
+    let u = Vec::<i32>::new();
     println!("The {}th fib is {}", n, y);
 }
